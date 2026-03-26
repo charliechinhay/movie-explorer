1fe87f2a594e9281791a5cce8f84b1f6a2fb5207
charlie.chinchay <hairolas123@hotmail.com>
2026-03-27 00:11:00 +0100

    sistem vercel json , and vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   root: "client",
   plugins: [react()],
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
 });
