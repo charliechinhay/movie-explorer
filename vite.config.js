e433180bd8b73c1f932a62a7c077a2665075a934
charlie.chinchay <hairolas123@hotmail.com>
2026-03-30 20:49:32 +0200

    gestion roots

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   root: "client",
   plugins: [react()],
+  resolve: {
+    dedupe: ["react", "react-dom", "react-router-dom"],
+  },
   build: {
     outDir: "../dist",
     emptyOutDir: true,
